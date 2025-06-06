efddf0a1ad59d5fa2e52cd92b6e9280cc084be0e
aryan4859 <aryan.shrestha@presidential.edu.np>
2025-06-06 13:16:37 -0400

    DB connection added :: mongoose,

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,6 @@ import "dotenv/config";
 
 export const config = {
   PORT: String(process.env.PORT),
+  MONGODB_URL: String(process.env.MONGODB_URL),
+  JWT_SECRET: String(process.env.JWT_SECRET),
 };
